c9116a353d569d8dd2abcbcc7d4ff88f69c8a002
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2019-12-13 00:22:57 +0200

    Update qutebrowser config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,24 @@
+import dracula.draw
+
+
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 2,
+        'horizontal': 8
+    },
+    'font': {
+        'family': 'Menlo, Terminus, Monaco, Monospace',
+        'size': 12
+    }
+})
+
+config.bind(',pp', 'spawn --userscript qute-pass')
+config.bind(',pL', 'spawn --userscript qute-pass --username-only')
+config.bind(',pP', 'spawn --userscript qute-pass --password-only')
+
+# with config.pattern('*://github.com/') as github:
+#     github.content.user_stylesheets = [
+#         'github-dark.css',
+#     ]
