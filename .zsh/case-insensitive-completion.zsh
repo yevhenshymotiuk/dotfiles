1cf6826837f3823a30b6d7292fffc7a7f5fcbb53
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-03-03 15:41:50 +0200

    Fix zsh case insensitive completions

--- .zsh/case-insensitive-completion.zsh
+++ .zsh/case-insensitive-completion.zsh
@@ -1,2 +1,1 @@
-zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' \
-    '+l:|?=** r:|?=**'
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
